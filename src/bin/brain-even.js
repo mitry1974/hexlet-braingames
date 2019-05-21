01072b0aaca46946fa2956ea93f82155c859c5a4
DT <tomitry@gmail.com>
2019-05-21 09:11:04 +0300

    small fixes in brain-even game

--- src/bin/brain-even.js
+++ src/bin/brain-even.js
@@ -1,25 +1,4 @@
 #!/usr/bin/env node
-import readlineSync from 'readline-sync';
+import brainEven from '../brain-even-body';
 
-const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min)) + min;
-const getNegAnswer = answer => (answer === 'yes' ? 'no' : 'yes');
-
-console.log('Welcome to the Brain Games!\nAnswer "yes" if number even otherwise answer "no".');
-const name = readlineSync.question('May I have your name? ');
-console.log(`Hello, ${name}!`);
-let bRes = false;
-for (let i = 0; i < 3; i += 1) {
-  const iQuery = getRandomInt(1, 100);
-  console.log(`Question: ${iQuery}`);
-  const answer = readlineSync.question('Your answer: ');
-  const bEven = iQuery % 2 === 0;
-  bRes = (bEven && answer === 'yes') || (!bEven && answer === 'no');
-  const infoString = bRes ? 'Correct!' : `'${answer}' is wrong answer ;(. Correct answer was '${getNegAnswer(answer)}'`;
-  console.log(infoString);
-  if (!bRes) {
-    break;
-  }
-}
-
-const finalString = bRes ? `Congratulations, ${name}!` : `Lets try again, ${name}!`;
-console.log(finalString);
+brainEven();
