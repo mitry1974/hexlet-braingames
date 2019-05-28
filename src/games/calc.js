4704f8690ac61aeb2b8813bf86ad2d907cca4730
DT <tomitry@gmail.com>
2019-05-28 22:13:25 +0300

    changes in calc game

--- src/games/calc.js
+++ src/games/calc.js
@@ -3,48 +3,27 @@
 import playGame from '../engine';
 import { getOperand, getRandomInt } from '../utils';
 
-const getCalculationResult = (operation) => {
-  let f;
-  switch (operation) {
-    case 1:
-      return (a, b) => a - b;
-    case 2:
-      return (a, b) => a + b;
-    case 3:
-      return (a, b) => a * b;
-    default:
-      break;
-  }
-  return f;
-};
+const minOperationNumber = 0;
+const maxOperationNumber = 3;
 
-const getOperationString = (operation) => {
-  let op = '';
-  switch (operation) {
-    case 1:
-      op = '-';
-      break;
-    case 2:
-      op = '+';
-      break;
-    case 3:
-      op = '*';
-      break;
-    default:
-      break;
-  }
-  return op;
-};
+const operations = [
+  ['-', (a, b) => a - b],
+  ['+', (a, b) => a + b],
+  ['*', (a, b) => a * b],
+];
 
 const calcGame = {
   description: 'What is the result of the expression?',
   getNextQuestion() {
     const opA = getOperand();
     const opB = getOperand();
-    const operation = getRandomInt(1, 3);
+    const [operationSymbol, operationFunction] = operations[getRandomInt(
+      minOperationNumber, maxOperationNumber,
+    )];
+
     return {
-      answer: (getCalculationResult(operation)(opA, opB)).toString(),
-      text: `${opA} ${getOperationString(operation)} ${opB} = `,
+      answer: (operationFunction(opA, opB)).toString(),
+      text: `${opA} ${operationSymbol} ${opB} = `,
     };
   },
 };
